40a5b84ddb19d346ccd0b7cf7575fd8f8177d637
Zaliant1 <ATMendoza1092@gmail.com>
2022-05-27 17:43:14 -0600

    replaced for loop with regex match

--- week1/friday/js-basics-1/password.js
+++ week1/friday/js-basics-1/password.js
@@ -9,21 +9,16 @@ const reader = readline.createInterface({
 
 console.log("welcome to the password validator tool");
 
-reader.question("give math equation separated by spaces ", (pass) => {
+reader.question("type out a password ", (pass) => {
   let lengthOfPassword = pass.length;
 
   // we check if the index cycling finds an uppercase
-  for (let i = 0; i < lengthOfPassword; i++) {
-    if (pass[i] === pass[i].toUpperCase()) {
-      console.log("password contains 1 uppercase");
 
-      break;
-      // else we wait til i indexes all of the characters to then give an exception to not bombard console log
-    } else if (i === lengthOfPassword - 1) {
-      console.log("password must contain at least 1 uppercase letter");
-    }
+  if (/[A-Z]/.test(pass)) {
+    console.log("password contains at least 1 uppercase");
+  } else {
+    console.log("password must contain at least 1 uppercase letter");
   }
-
   // we check if the password length is good
   if (lengthOfPassword < 10) {
     console.log("password must contain at least 10 characters");
